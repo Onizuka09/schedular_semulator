a0dfb478a12717d902e65d5e2b8a89474684a0d5
Onizuka09 <smokthar925@gmail.com>
2023-12-08 15:09:16 +0100

    added the calculate simulation time to the golbal conf

--- process_config/global_config.c
+++ process_config/global_config.c
@@ -1,24 +1,4 @@
 #include "global_config.h"
-/*
-	void get_userInput(Process * proc)
-	{
-		// proc->name = malloc(255);
-		int cl = 0;
-		printf("enter name: ");
-		scanf("%s", proc->name);
-		printf("enter ta: ");
-		scanf("%d", &proc->ta);
-		printf("enter te: ");
-		scanf("%d", &proc->te);
-		printf("enter priority: ");
-		scanf("%d", &proc->priority);
-		printf("enter color: ");
-		scanf("%d", &cl);
-		proc->color = intToColor(cl);
-		printf("\n");
-		return;
-	}
-*/
 
 int my_round(float num)
 {
@@ -28,4 +8,65 @@ int my_round(float num)
 	else
 		return floor(num) + 1;
 }
-	
+void init_rand(void)
+{
+	srand((unsigned int)time(NULL));
+}
+
+int generate_num_proc(void)
+{
+	// int time();
+
+	int nb_proc = (rand() % MAX_num_proc) + 1;
+	return nb_proc;
+}
+int generate_color(int *tab,int nb_proc){
+	// int time();
+	// srand((unsigned int)time(NULL));
+	int color;
+	color = (rand() % TOTAL_CL )+ 1;
+	for (int i = 0; i < nb_proc; i++)
+	{ 
+		if (*(tab+i)==color)
+		{
+			i = 0;
+			color = (rand() % TOTAL_CL) + 1;
+			continue;
+		}
+	}
+	color <= 0 ? color = 1 : color;
+	return color;
+}
+void  random_process_generation(Process* proc,int nb_proc)
+{
+	// int time();
+	// srand((unsigned int)time(NULL));
+	proc->te = (rand() % max_TE) + 1;
+	proc->te <= 0 ? proc->te = 1 : proc->te;
+	// Arrival date
+	proc->ta = (rand() % max_TA);
+	proc->ta <= 0 ? proc->ta = 1 : proc->ta;
+	// Priority
+	proc->priority = rand() % nb_proc + 1;
+	proc->priority <= 0 ? proc->priority = 1 : proc->priority;
+}
+
+int calculate_simulation_time(node *head)
+{
+	int total_t = 0;
+	node *n;
+	Process p;
+	n = head;
+	while (n != NULL)
+	{
+		p = n->proc;
+		if (total_t < p.ta)
+		{
+			total_t = p.ta;
+		}
+		total_t += p.te;
+		n = n->next;
+	}
+	free(n);
+	return total_t;
+}
--- process_config/global_config.h
+++ process_config/global_config.h
@@ -2,16 +2,23 @@
 #define CONFIG_H
 // #include"../display_manger/display_conf.h"
 #include "../process_def.h" 
+#include "../dataStruct/node.h"
 // #include"../dataStruct/queue.h"
 #include <math.h>
-
-
+#include <stddef.h>
+#include<stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 // void get_userInput(Process* proc);
 // void printTable(queue *q1,int num_proc);
 // void printTable_linkedList(node *head, int num_proc);
 // void printTable_view(Process *proc, int num_proc);
-void random_process_generation(Process *proc);
+void random_process_generation(Process *proc,int nb_proc);
+int generate_color(int *tab, int nb_proc);
 int my_round(float num);
+int generate_num_proc(void);
+void init_rand(void);
+int calculate_simulation_time(node *head);
 
 /*----------------------------------------*/
 
