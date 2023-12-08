33871165c86f7ab6220509086bd96ad79ee569b5
Amani Salah <salah.amani2001@gmail.com>
2023-12-08 14:13:18 +0100

    Handling situations where two processes share the same priority

--- dataStruct/linkedlist.c
+++ dataStruct/linkedlist.c
@@ -96,6 +96,15 @@ int linkedlist_bubbleSortpriority( node** head, int count)
                 *h = swap(p1, p2);
                 swapped = 1;
             }
+            else if (p1->proc.priority = p2->proc.priority){
+                if (p1->proc.ta > p2->proc.ta) {
+
+                /* update the link after swapping */
+                *h = swap(p1, p2);
+                swapped = 1;
+            }
+
+            }
 
             h = &(*h)->next;
         }
