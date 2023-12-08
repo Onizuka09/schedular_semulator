ddc3eea6355ea69349b3a2cec86ec2a12e19f278
Onizuka09 <smokthar925@gmail.com>
2023-12-08 15:04:43 +0100

    added a new module to handle the file operations

---
+++ file_manipulation/csv_file_manip.h
@@ -0,0 +1,19 @@
+#ifndef _CSV_FILE_MANIP_H_
+#define _CSV_FILE_MANIP_H_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../process_def.h"
+#include "../process_config/global_config.h"
+#include "../dataStruct/linkedlist.h"
+#include "../display_manger/display_conf.h"
+
+bool Create_CSV_file(char *filename);
+int generate_num_proc(void) ;
+bool fill_csv_file(char *file_name);
+node* Read_csv_file(char *file_name,int *nbr);
+bool is_file_exits(char *file_name);
+bool remove_file();
+
+#endif
