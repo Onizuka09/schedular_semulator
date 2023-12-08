26889ba7d4f27bbb61c09d67ae4f6fa89bc53185
Onizuka09 <smokthar925@gmail.com>
2023-12-08 16:22:32 +0100

    update

--- test_colors.c
+++ test_colors.c
@@ -15,27 +15,48 @@
 #define BRIGHT_BLUE     	"\x1b[94m"
 #define BRIGHT_PURPLE       "\x1b[95m"
 #define BRIGHT_CYAN     	"\x1b[96m"
-
+typedef enum
+{
+	E_RESET_C,
+	E_BLACK_C,
+	E_GREY_C,
+	E_RED_C,
+	E_GREEN_C,
+	E_YELLOW_C,
+	E_BLUE_C,
+	E_PURPLE_C,
+	E_CYAN_C,
+	E_BRIGHT_GREY_C,
+	E_BRIGHT_RED_C,
+	E_BRIGHT_GREEN_C,
+	E_BRIGHT_YELLOW_C,
+	E_BRIGHT_BLUE_C,
+	E_BRIGHT_PURPLE_C,
+	E_BRIGHT_CYAN_C,
+	TOTAL_COLORS,
+} colors;
+#define TOTAL_CL (int)TOTAL_COLORS-1
 int main(void)
 {
 	int i, j, n;
 // printf("")
-printf(BLACK "hello world\n" RESET);
+	printf("num: %d \n ", TOTAL_CL);
+	printf(BLACK "hello world\n" RESET);
 
-printf(GREY "hello world\n" RESET);
-printf(RED "hello world\n" RESET);
-printf(GREEN "hello world\n" RESET);
-printf(YELLOW "hello world\n" RESET);
-printf(BLUE "hello world\n" RESET);
-printf(PURPLE "hello world\n" RESET);
-printf(CYAN "hello world\n" RESET);
-printf(BRIGHT_GREY "hello world\n" RESET);
-printf(BRIGHT_RED "hello world\n" RESET);
-printf(BRIGHT_GREEN "hello world\n" RESET);
-printf(BRIGHT_YELLOW "hello world\n" RESET);
-printf(BRIGHT_BLUE "hello world\n" RESET);
-printf(BRIGHT_PURPLE "hello world\n" RESET);
-printf(BRIGHT_CYAN "hello world\n" RESET);
-printf(RESET);
-return 0;
+	printf(GREY "hello world\n" RESET);
+	printf(RED "hello world\n" RESET);
+	printf(GREEN "hello world\n" RESET);
+	printf(YELLOW "hello world\n" RESET);
+	printf(BLUE "hello world\n" RESET);
+	printf(PURPLE "hello world\n" RESET);
+	printf(CYAN "hello world\n" RESET);
+	printf(BRIGHT_GREY "hello world\n" RESET);
+	printf(BRIGHT_RED "hello world\n" RESET);
+	printf(BRIGHT_GREEN "hello world\n" RESET);
+	printf(BRIGHT_YELLOW "hello world\n" RESET);
+	printf(BRIGHT_BLUE "hello world\n" RESET);
+	printf(BRIGHT_PURPLE "hello world\n" RESET);
+	printf(BRIGHT_CYAN "hello world\n" RESET);
+	printf(RESET);
+	return 0;
 }
